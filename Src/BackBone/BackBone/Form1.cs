70620b695b238ee6ba5504732a5f1d54744bf358
Michaël André Franiatte <113953032+michaelandrefraniatte@users.noreply.github.com>
2023-09-20 11:21:17 +0200

    Fix a bug

--- Src/BackBone/BackBone/Form1.cs
+++ Src/BackBone/BackBone/Form1.cs
@@ -91,7 +91,7 @@ namespace BackBone
             try
             {
                 bitmap = PrintWindow(findwindow);
-                bitmap = new Bitmap(bitmap, new Size(bitmap.Width / 4, bitmap.Height / 4));
+                bitmap = new Bitmap(bitmap, new Size(bitmap.Width / 3, bitmap.Height / 3));
                 if (type == "1")
                 {
                     bitmap = new Bitmap(bitmap, new Size(bitmap.Width / 2, bitmap.Height / 2));
